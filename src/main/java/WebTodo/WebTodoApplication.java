26eb6a9feb6cb86ac333b920a87b1f876b874616
MinhDuc <duc11t3bk@gmail.com>
2020-02-24 21:38:31 +0900

    init

---
+++ src/main/java/WebTodo/WebTodoApplication.java
@@ -0,0 +1,13 @@
+package WebTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WebTodo/WebTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package WebTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
